cf55bff8063d5caf00e86036feca47b04c43ca0f
thekill022 <gibfathoni@gmail.com>
2024-11-23 22:24:06 +0700

    membuat tampilan index.ejs dan membuat session

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
 const bodyParser = require('body-parser')
 
-app.set("view engine", "ejs")
 
 require('dotenv').config()
 const port = process.env.PORT
@@ -10,9 +10,31 @@ const port = process.env.PORT
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended:true}))
 
+// menambahkan user session
+app.use(session({
+    secret : 'Admin#123', //kyk kominfo hehe
+    resave : false,
+    saveUninitialized : false,
+    cookie : {
+        maxAge : 600000 * 10
+    }
+}))
+
+// membuat middleware untuk validasi login berdasarkan session
+function isAuthenticated(req, res, next) {
+    if (req.session && req.session.user) {
+        return next()
+    } else {
+        res.redirect('/')
+    }
+}
+
+app.set("view engine", "ejs")
+
 app.get('/', (req, res) => {
     res.render('index')
 })
+
 
 app.listen(port, () => {
     console.log(`app listen on link : http://localhost:${port}`)
