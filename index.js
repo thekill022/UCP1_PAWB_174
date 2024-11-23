4c64373666b5ad7d14d8f1201a3f4248dc34fd10
thekill022 <gibfathoni@gmail.com>
2024-11-23 21:51:30 +0700

    set view engine, body parser, dan urlencoded

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.set("view engine", "ejs")
 
 require('dotenv').config()
 const port = process.env.PORT
 
+app.use(bodyParser.json())
+app.use(express.urlencoded({extended:true}))
+
 app.get('/', (req, res) => {
-    res.send('hello World')
+    res.render('index')
 })
 
 app.listen(port, () => {
